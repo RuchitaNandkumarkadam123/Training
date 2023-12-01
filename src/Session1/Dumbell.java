eca610a8d24f925f48e48f392df5e6c67d439e41
Ruchita kadam <ruchitakadam267@gmail.com>
2023-11-30 18:51:36 -0700

    My first class creation program

---
+++ src/Session1/Dumbell.java
@@ -0,0 +1,12 @@
+package Session1;
+
+public class Dumbell {
+	
+	double Size;
+	double weight;
+	String color;
+	
+    void Toworkout() {
+    	System.out.println("I love doing workout");
+    }
+}
