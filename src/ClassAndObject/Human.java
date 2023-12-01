0bb137ad76d27a8f3045e58298e287fb8763da0b
Ruchita kadam <ruchitakadam267@gmail.com>
2023-11-30 19:23:11 -0700

    My fourth class program

---
+++ src/ClassAndObject/Human.java
@@ -0,0 +1,21 @@
+package ClassAndObject;
+
+public class Human {
+   
+	String Name;
+	double Height;
+	double Weight;
+	String Color;
+	
+	void speak(){
+		System.out.println("Human loves to speak");
+	}
+	
+	void walk() {
+		System.out.println("Human enjoy daily walk");
+	}
+
+	void cry() {
+		System.out.println("Human can cry");
+	}
+}
