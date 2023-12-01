076ba6d71a9bf3a4437c614b145f6199dc84e309
Ruchita kadam <ruchitakadam267@gmail.com>
2023-11-30 19:00:29 -0700

    My second class program

---
+++ src/ClassAndObject/Bluetooth.java
@@ -0,0 +1,14 @@
+package ClassAndObject;
+
+public class Bluetooth {
+	
+	int Size;
+	String color;
+	String brand;
+	int storage;
+	
+	void DataStorage() {
+		System.out.println("Pendrive is use to store the data");
+	}
+
+}
