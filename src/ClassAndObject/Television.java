f4390c597e9194e28d50cca53ffb6fa16407e722
Ruchita kadam <ruchitakadam267@gmail.com>
2023-11-30 19:15:37 -0700

    My third class programm

---
+++ src/ClassAndObject/Television.java
@@ -0,0 +1,16 @@
+package ClassAndObject;
+
+public class Television {
+	
+	int Size;
+	String brand;
+	String Type;
+	
+	void ToWatch() {
+		System.out.println("Televison is use to watch movie");
+	}
+    
+	void Entertainment() {
+		System.out.println("TV is for entertainment");
+	}
+}
