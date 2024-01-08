0768ce44e4922e7629bf28258de6f6a2e2abaf32
Ruchita kadam <ruchitakadam267@gmail.com>
2024-01-08 10:03:01 -0700

    String 2 commit

---
+++ src/string/string1.java
@@ -0,0 +1,13 @@
+package string;
+
+public class string1 {
+public static void main(String[] args) {
+	
+	String a="this is java";
+	System.out.println(a.replace(" ", "").length());
+	
+	}
+	
+	
+}
+
