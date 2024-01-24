24c22c464060ebefc0c2c98ede5e3e26ad7064d8
Ruchita kadam <ruchitakadam267@gmail.com>
2024-01-24 07:57:28 -0700

    commit

--- src/oops/VariableScope.java
+++ src/oops/VariableScope.java
@@ -4,9 +4,10 @@ public class VariableScope {
        public static void main(String[] args) {
     	   Demo5 D5=new Demo5();
     	   D5.add();
-    	   //Demo5.j;    //while calling static variable i am getting error can u please help me with that 
-	}
+    	   //Demo5;  //while calling static variable i am getting error can u please help me with that 
+       } 
 }
+    	 
  class Demo5{
 	 
 	int i =10;          //instance variable 
