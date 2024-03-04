7b61d22c8b0bea5bcfafd6f9a6cb1dd5b260a41e
Ruchita kadam <ruchitakadam267@gmail.com>
2024-03-03 18:37:08 -0700

    Commit

---
+++ src/question/Test.java
@@ -0,0 +1,13 @@
+package question;
+
+public class Test {
+  static int x;
+  public static void main(String[] args) {
+	System.out.println(fun());
+}
+  public static  int fun() {
+	  return ++x;
+	  
+  }
+}
+//Static method need to be called in the sttaic method 
